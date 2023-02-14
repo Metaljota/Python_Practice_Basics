6bd83fc10e8ed084025ad01a7d558235178ebd51
Jay <105905548+Metaljota@users.noreply.github.com>
2023-02-14 17:54:10 +1100

    Other exercise

---
+++ Your remaing days until 90 years.py
@@ -0,0 +1,15 @@
+# print("--------------")
+# print("DAYS UNTIL YOUR 90s CALCULATOR")
+# print("--------------")
+
+# age = input("What is your current age? ")
+
+
+# days = 32850 - (int(age) * 365)
+# weeks = 4680 - (int(age) * 52)
+# months = 1080 - (int(age) * 12)
+
+# print(f"You have {days} days, {weeks} weeks, and {months} months left.")
+
+a = int("5") / int(2.7)
+print(a) 
